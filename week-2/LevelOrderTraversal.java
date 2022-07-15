fc62142371c001cc60e43a24e7a433412f92115d
Kartikey Tyagi <59220409+kartikey110813@users.noreply.github.com>
2022-07-15 09:45:03 +0530

    Create LevelOrderTraversal.java

---
+++ week-2/LevelOrderTraversal.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> ans = new ArrayList<>();
+            while (n > 0) {
+                n--;
+                TreeNode node = queue.remove();
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+                ans.add(node.val);
+            }
+            res.add(ans);
+        }
+        return res;
+    }
+}
