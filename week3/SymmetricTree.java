b1d601c4acfc2168efd7501f11ff9e5e93ed6e2d
Kartikey Tyagi <59220409+kartikey110813@users.noreply.github.com>
2022-07-22 09:33:43 +0530

    Create SymmetricTree.java

---
+++ week3/SymmetricTree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        
+        if(root == null) return true;
+        
+        return isMirror(root.left, root.right);
+        
+    }
+    
+    public boolean isMirror(TreeNode l, TreeNode r){
+        if(l == null && r == null) return true;
+        if(l == null && r!=null) return false;
+        if(l!=null && r==null) return false;
+        if(l.val != r.val) return false; 
+
+        
+        return isMirror(l.left,r.right) && isMirror(l.right,r.left);
+    }
+}
