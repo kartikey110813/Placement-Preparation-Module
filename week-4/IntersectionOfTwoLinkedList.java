8d583456db4137eea27f549935cc28061b47ea07
Kartikey Tyagi <59220409+kartikey110813@users.noreply.github.com>
2022-07-28 09:37:53 +0530

    Create IntersectionOfTwoLinkedList.java

---
+++ week-4/IntersectionOfTwoLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) {
+            return null;
+        }
+        
+        ListNode l1 = headA;
+        ListNode l2 = headB;
+        
+        while(l1 != l2){
+            l1 = (l1 == null) ? headB : l1.next;
+            l2 = (l2 == null) ? headA : l2.next;
+        }
+        
+        return l1;
+    }
+}
