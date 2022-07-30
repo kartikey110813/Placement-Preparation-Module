e7aa3bbde09c0b71f207a10f35ba449af32a11a3
Kartikey Tyagi <59220409+kartikey110813@users.noreply.github.com>
2022-07-30 08:44:33 +0530

    Create BalancedBinaryTree.java

---
+++ week3/BalancedBinaryTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     boolean balanced = true;
+    public boolean isBalanced(TreeNode root) {
+       
+        helper(root);
+        
+        return balanced;
+    }
+    
+    public int helper(TreeNode root){
+        if(root == null)
+            return 0;
+        
+        int left = helper(root.left);
+        int right =  helper(root.right);
+        
+        if(Math.abs(right-left) > 1){
+            balanced = false;
+        }
+        
+        
+        return Math.max(left,right) +1;
+    }
+}
