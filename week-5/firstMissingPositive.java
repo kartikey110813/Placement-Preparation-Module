1f5efddbbfb72fe11e5b60a654403ed4f8d79925
Kartikey Tyagi <59220409+kartikey110813@users.noreply.github.com>
2022-08-08 09:01:22 +0530

    Create firstMissingPositive.java

---
+++ week-5/firstMissingPositive.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+           
+        int n = nums.length;
+        int i = 0;
+        while(i < n){
+            int corect = nums[i] - 1;
+            if(nums[i] > 0 && nums[i] <= n && nums[i] != nums[corect]){
+                int temp = nums[i];
+                nums[i] = nums[corect];
+                nums[corect] = temp;
+            }
+            else
+                i++;
+            }
+        
+        for(int j = 0;j<n;j++){
+            if(nums[j] != j + 1)
+                return j+1;
+        }
+        
+        return n+1;
+    }
+}
